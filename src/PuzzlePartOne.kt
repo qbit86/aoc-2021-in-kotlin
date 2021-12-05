6827124cc3cb4722025135f4859fc88a9f1ae76f
Viktor T <qbit86@gmail.com>
2021-12-05 22:44:32 +0300

    Implement Part One

--- src/PuzzlePartOne.kt
+++ src/PuzzlePartOne.kt
@@ -1,5 +1,14 @@
 object PuzzlePartOne {
     fun solve(inputLines: List<String>): Int {
-        TODO()
+        val drawInput: String = inputLines[0]
+        val draw = Draw.create(drawInput)
+
+        val boardInputs: List<List<String>> = inputLines.drop(1).chunked(6).map { it.drop(1) }
+        val boards: List<Board> = boardInputs.map { Board.create(it) }
+
+        val boardTurnPairs: List<Pair<Board, Int>> = boards.map { it to it.computeWinningTurn(draw) }
+        val (firstWinningBoard, turn) = boardTurnPairs.minByOrNull { it.second }!!
+
+        return firstWinningBoard.computeScore(draw, turn)
     }
 }
