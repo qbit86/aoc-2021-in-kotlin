26c0c5445cd0b94547c326eafc57ddedbf277611
Viktor T <qbit86@gmail.com>
2021-12-05 22:48:07 +0300

    Add test case for Part Two

--- src/Runner.kt
+++ src/Runner.kt
@@ -1,7 +1,12 @@
 fun main() {
+    // https://adventofcode.com/2021/day/4
+
     val testInputLines = readInput("test-input")
     val inputLines = readInput("input")
 
     check(PuzzlePartOne.solve(testInputLines) == 4512)
     println(PuzzlePartOne.solve(inputLines))
+
+    check(PuzzlePartTwo.solve(testInputLines) == 1924)
+    println(PuzzlePartTwo.solve(inputLines))
 }
