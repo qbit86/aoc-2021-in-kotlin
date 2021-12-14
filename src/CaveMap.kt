6db4b0822b5749c3cb14be684abdeb304d0f3469
Viktor T <qbit86@gmail.com>
2021-12-15 02:26:30 +0300

    Add CaveMap

---
+++ src/CaveMap.kt
@@ -0,0 +1,25 @@
+class CaveMap private constructor(private val neighborsByCave: Map<String, Collection<String>>) {
+    fun getNeighborsByCave(cave: String): Collection<String> = neighborsByCave[cave].orEmpty()
+
+    companion object {
+        internal fun create(inputLines: List<String>): CaveMap {
+            val endpointsList: List<List<String>> = inputLines.map { it.split("-") }
+
+            val neighborsByCave: MutableMap<String, MutableSet<String>> = mutableMapOf()
+            fun addEdge(tail: String, head: String) {
+                val neighbors = neighborsByCave[tail]
+                if (neighbors != null)
+                    neighbors.add(head)
+                else
+                    neighborsByCave[tail] = mutableSetOf(head)
+            }
+
+            for (endpoints in endpointsList) {
+                addEdge(endpoints[0], endpoints[1])
+                addEdge(endpoints[1], endpoints[0])
+            }
+
+            return CaveMap(neighborsByCave)
+        }
+    }
+}
