1ac5f4572fcac782f2c4121bae35bf779e8afd32
Viktor T <qbit86@gmail.com>
2021-12-15 02:32:53 +0300

    Implement Part One

--- src/PuzzlePartOne.kt
+++ src/PuzzlePartOne.kt
@@ -1,5 +1,35 @@
 object PuzzlePartOne {
     fun solve(inputLines: List<String>): Int {
-        TODO()
+        val caveMap = CaveMap.create(inputLines)
+
+        fun getPathCount(cave: String, explored: MutableSet<String>): Int {
+            if (CaveMap.isSmall(cave))
+                explored.add(cave)
+            try {
+                var result = 0
+                val neighbors = caveMap.getNeighborsByCave(cave)
+                for (neighbor in neighbors) {
+                    if (neighbor == "end") {
+                        ++result
+                        continue
+                    }
+
+                    if (CaveMap.isBig(neighbor)) {
+                        result += getPathCount(neighbor, explored)
+                        continue
+                    }
+
+                    if (!explored.contains(neighbor))
+                        result += getPathCount(neighbor, explored)
+                }
+
+                return result
+            } finally {
+                if (CaveMap.isSmall(cave))
+                    explored.remove(cave)
+            }
+        }
+
+        return getPathCount("start", mutableSetOf())
     }
 }
