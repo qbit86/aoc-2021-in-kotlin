a49a0364966ae87b106ccdada8b484355f33e2b9
Viktor T <qbit86@gmail.com>
2021-12-22 23:08:35 +0300

    Add test input

--- src/Runner.kt
+++ src/Runner.kt
@@ -2,6 +2,9 @@ fun main() {
     val testInputLines = readInput("test-input")
     val inputLines = readInput("input")
 
-    check(PuzzlePartOne.solve(testInputLines) == 0)
+    check(PuzzlePartOne.solve(testInputLines) == 35)
     println(PuzzlePartOne.solve(inputLines))
+
+    check(PuzzlePartTwo.solve(testInputLines) == 3351)
+    println(PuzzlePartTwo.solve(inputLines))
 }
